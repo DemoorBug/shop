923cfb588f3646054cd7f8887a61f822c337d9ae
demoorbug <demoorbug@gmail.com>
2019-03-19 01:13:13 +0800

    lunbo

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -10,8 +10,8 @@ import '@/css/font/iconfont.css'
 /*公共样式*/
 import "@/css/common.less";
 
-import '~node/slick-carousel/slick/slick.css';
-import '~node/slick-carousel/slick/slick-theme.css';
+// import '~node/slick-carousel/slick/slick.css';
+// import '~node/slick-carousel/slick/slick-theme.css';
 
 //////////////
 // main.css //
@@ -19,28 +19,42 @@ import '~node/slick-carousel/slick/slick-theme.css';
 
 import "./main.less";
 
-import '~node/jquery/dist/jquery.js';
+// import '~node/jquery/dist/jquery.js';
 
-import '~node/slick-carousel/slick/slick.min.js';
+// import '~node/slick-carousel/slick/slick.min.js';
 
-$('.single-item').slick({
-  accessibility: false,
-  autoplay: true,
-  arrows: false,
-  dots: true,
-  appendDots: $('.slider-Dots'),
-  fade: true,
-  cssEase: 'linear',
-  speed: 500,
-});
+import './js/slider.me.js'
 
-$(function(){
-
-
-  $('.single-item').on('beforeChange', (event, slick, currentSlide, nextSlide) => {
-    let bg_color = $(".img").eq(nextSlide).data('img');
-    $('.slider-banner').css({
-      'background' : bg_color
-    })
+$(function() {
+  var bannerSlider = new Slider($('.slider_tabs'), {
+    time: 5000,
+    delay: 400,
+    event: 'hover',
+    auto: true,
+    mode: 'fade',
+    controller: $('.slider-Dots'),
+    activeControllerCls: 'active'
   });
 })
+
+// $('.single-item').slick({
+//   accessibility: false,
+//   autoplay: true,
+//   arrows: false,
+//   dots: true,
+//   appendDots: $('.slider-Dots'),
+//   fade: true,
+//   cssEase: 'linear',
+//   speed: 500,
+// });
+
+// $(function(){
+
+    // $('.single-item').on('beforeChange', (event, slick, currentSlide, nextSlide) => {
+    //   let bg_color = $(".img").eq(nextSlide).data('img');
+    //   $('.slider-banner').css({
+    //     'background' : bg_color
+    //   })
+    // });
+
+// })
